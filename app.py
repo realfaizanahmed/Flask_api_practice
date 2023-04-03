a9ba553911a821fe452594b96ba4e61b3905b196
realfaizanahmed <112700497+realfaizanahmed@users.noreply.github.com>
2023-04-03 17:55:52 +0500

    Setup the database for Flask API practice

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Initialize App
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Initialize db
+db = SQLAlchemy(app)
+
+#Initialize marshmallow
+ma = Marshmallow(app)
+
+#Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+#Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
